6e17b017988ca59f473f3808ace787d8c177a527
Mark Reynolds <93389291+MarkJMReynolds-SAG@users.noreply.github.com>
2023-12-01 11:13:18 +0000

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 
 class HelloWorld {
+
+  //Hopefully CodeQL will spot this... also what happens if I mention stackoverflow.com?
+  String password = "supersecretvalue"; 
+  
   public static void main(String argv[]) {
     System.out.println("Hello World");
   }
